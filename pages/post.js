0184ecd5c2f0fb652eb15a8a620f91f481d9b1da
Daijiro Wachi <daijiro.wachi@gmail.com>
2018-12-28 16:45:36 +0900

    use withRouter

--- pages/post.js
+++ pages/post.js
@@ -1,10 +1,16 @@
 import {withRouter} from 'next/router'
 import Layout from '../components/MyLayout.js'
 
+const Content = withRouter((props) => (
+  <div>
+    <h1>{props.router.query.title}</h1>
+    <p>This is the blog post content.</p>
+  </div>
+))
+
 const Page = withRouter((props) => (
     <Layout>
-       <h1>{props.router.query.title}</h1>
-       <p>This is the blog post content.</p>
+       <Content />
     </Layout>
 ))
 
